8296735cc47fee432400747924f80afb8f2259be
Rohini Shirsat <shirsatrohini66@gmail.com>
2022-07-20 15:35:23 +0530

    Updated setting.py

--- blog_pro/settings.py
+++ blog_pro/settings.py
@@ -16,21 +16,23 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR=os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 env=environ.Env(SECRET_KEY=str,)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # SECURITY WARNING: don't run with debug turned on in production!
-
-
-
-
-
 # Application definition
+SECRET_KEY=env('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG=env('DJANGO_DEBUG')
+
+ALLOWED_HOSTS=[env('DJANGO_ALLOWED_HOSTS')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
